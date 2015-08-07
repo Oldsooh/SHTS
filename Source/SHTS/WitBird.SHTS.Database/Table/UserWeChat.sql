fd8a4b667a01f5172b6e01ef34f027ce0ef399cb
osooh <osooh@outlook.com>
2015-08-07 23:48:15 +0800

    Adds table which saves wechat user infomation

---
+++ Source/SHTS/WitBird.SHTS.Database/Table/UserWeChat.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[UserWeChat]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NULL, 
+    [WeChatId] NVARCHAR(50) NULL, 
+    [OpenId] NVARCHAR(50) NULL, 
+    [NickName] NVARCHAR(50) NULL, 
+    [Sex] INT NULL DEFAULT 0, 
+    [Province] NVARCHAR(50) NULL, 
+    [City] NVARCHAR(50) NULL, 
+    [County] NVARCHAR(50) NULL, 
+    [Photo] NVARCHAR(50) NULL, 
+    [AcccessToken] NVARCHAR(50) NULL, 
+    [AccessTokenExpired] BIT NULL DEFAULT 0, 
+    [AccessTokenExpireTime] DATETIME NULL, 
+    [State] INT NULL, 
+    [CreatedTime] DATETIME NULL, 
+    CONSTRAINT [FK_UserWeChat_User] FOREIGN KEY (UserId) REFERENCES [User]([UserId])
+)
