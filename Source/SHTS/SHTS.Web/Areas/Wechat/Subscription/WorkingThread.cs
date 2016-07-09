9db198be481f146e7ac0308ba1b79f28e24c1205
richard <richard@osooh.com>
2016-07-09 20:45:59 +0800

    重构数据库解决方案文件目录

---
+++ Source/SHTS/SHTS.Web/Areas/Wechat/Subscription/WorkingThread.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Witbird.SHTS.Web.Areas.Wechat.Subscription
+{
+    /// <summary>
+    /// 微信推送管理线程
+    /// </summary>
+    public static class WorkingThread
+    {
+    }
+}
