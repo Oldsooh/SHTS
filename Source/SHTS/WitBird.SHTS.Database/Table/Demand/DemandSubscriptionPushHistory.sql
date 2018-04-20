5a5bde9fd3e2f0aee5ff010fde08cb22d286a80f
Richard.Huang <richard@osooh.com>
2018-04-20 15:31:03 +0800

    Fix bug: save demand push history failed due to table feild length is too short

--- Source/SHTS/WitBird.SHTS.Database/Table/Demand/DemandSubscriptionPushHistory.sql
+++ Source/SHTS/WitBird.SHTS.Database/Table/Demand/DemandSubscriptionPushHistory.sql
@@ -2,13 +2,13 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [WechatUserId] INT NOT NULL, 
-    [OpenId] NVARCHAR(50) NOT NULL, 
+    [OpenId] NVARCHAR(100) NOT NULL, 
     [IsMailSubscribed] BIT NOT NULL, 
-    [EmailAddress] NVARCHAR(100) NULL, 
+    [EmailAddress] NVARCHAR(200) NULL, 
     [DemandId] INT NOT NULL, 
-    [WechatStatus] NVARCHAR(50) NOT NULL, 
-	[WechatExceptionMessage] NVARCHAR(500) NULL,
-    [EmailStatus] NVARCHAR(50) NULL, 
-	[EmailExceptionMessage] NVARCHAR(500) NULL,
+    [WechatStatus] NVARCHAR(500) NOT NULL, 
+	[WechatExceptionMessage] NVARCHAR(MAX) NULL,
+    [EmailStatus] NVARCHAR(500) NULL, 
+	[EmailExceptionMessage] NVARCHAR(MAX) NULL,
     [CreatedDateTime] DATETIME NOT NULL
 )
