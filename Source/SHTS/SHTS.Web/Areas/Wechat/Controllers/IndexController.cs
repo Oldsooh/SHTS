9cbcd381453e27bf8dd1c871aa7f0c15907dc98b
Richard.Huang <richard@osooh.com>
2018-04-29 12:36:41 +0800

    add subscribe reqiured page for wechat

--- Source/SHTS/SHTS.Web/Areas/Wechat/Controllers/IndexController.cs
+++ Source/SHTS/SHTS.Web/Areas/Wechat/Controllers/IndexController.cs
@@ -94,5 +94,14 @@ namespace Witbird.SHTS.Web.Areas.Wechat.Controllers
 
             return View(viewModel);
         }
+
+        /// <summary>
+        /// 自定义关注页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SubscribeRequired()
+        {
+            return View();
+        }
     }
 }
